4d71489d7ca6f9ce49191d42bebfc353101f44e4
zhangkaichao <zhangkc@kcwl.com>
2022-11-07 11:32:45 +0800

    feat: add

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [1, 'always'],
-    'header-max-length': [2, 'always', 108],
+    // 'body-leading-blank': [2, 'always'],
+    // 'footer-leading-blank': [1, 'always'],
+    // 'header-max-length': [2, 'always', 108],
     'subject-empty': [2, 'never'],
     'type-empty': [2, 'never'],
     'type-enum': [
