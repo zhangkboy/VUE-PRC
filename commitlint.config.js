f15379856d32d08b2720911021941f438e5abfb0
zhangkaichao <zhangkc@kcwl.com>
2022-11-07 13:10:17 +0800

    feat: test commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -31,7 +31,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-leading-blank': [2, 'always'],
+    // 'body-leading-blank': [2, 'always'],
     // 'footer-leading-blank': [1, 'always'],
     // 'header-max-length': [2, 'always', 108],
     'subject-empty': [2, 'never'],
